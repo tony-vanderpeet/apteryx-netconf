7f87830382337a5657aec0fd128df453e58c8357
Carl Smith <carl.smith@alliedtelesis.co.nz>
2021-11-25 14:50:25 +1300

    Fixup paths for local apteryx library build

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 ROOT=`pwd`
 
 # Check required libraries and tools
-if ! pkg-config --exists glib-2.0 libxml-2.0 cunit; then
+if ! pkg-config --exists glib-2.0 libxml-2.0 cunit jansson; then
         echo "Please install glib-2.0, libxml-2.0 and cunit"
-        echo "(sudo apt-get install libglib2.0-dev libxml2-dev libcunit1-dev)"
+        echo "(sudo apt-get install libglib2.0-dev libxml2-dev libcunit1-dev libjansson-dev)"
         exit 1
 fi
 
@@ -15,11 +15,12 @@ cd $BUILD
 
 # Check Apteryx install
 if [ ! -d apteryx ]; then
-        echo "Building Apteryx from source."
+        echo "Downloading Apteryx"
         git clone --depth 1 https://github.com/alliedtelesis/apteryx.git
         rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 fi
 if [ ! -f apteryx/libapteryx.so ]; then
+        echo "Building Apteryx"
         cd apteryx
         make install DESTDIR=$BUILD
         rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
@@ -28,26 +29,28 @@ fi
 
 # Check Apteryx XML Schema library
 if [ ! -d apteryx-xml ]; then
-        echo "Building libyang from source."
+        echo "Downloading apteryx-xml"
         git clone --depth 1 https://github.com/alliedtelesis/apteryx-xml.git
         rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 fi
 if [ ! -f apteryx-xml/libapteryx-schema.so ]; then
+        echo "Building apteryx-xml"
         cd apteryx-xml
         rm -f $BUILD/usr/lib/libapteryx-xml.so
         rm -f $BUILD/usr/lib/libapteryx-schema.so
-        make install DESTDIR=$BUILD
+        make install DESTDIR=$BUILD APTERYX_PATH=$BUILD/apteryx
         rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
         cd $BUILD
 fi
 
 # Check openssh
 if [ ! -d openssh ]; then
-        echo "Building openssh from source."
+        echo "Downloading openssh"
         git clone --depth 1 --branch V_8_8_P1 git://anongit.mindrot.org/openssh.git
         rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 fi
 if [ ! -f openssh/sshd ]; then
+        echo "Building openssh"
         cd openssh
         autoreconf -fvi
         ./configure --prefix=/usr --with-privsep-path=$BUILD/empty --with-privsep-user=manager
@@ -69,13 +72,15 @@ Subsystem netconf /usr/bin/socat STDIO UNIX:$BUILD/apteryx-netconf.sock
 " > $BUILD/sshd_config
 
 # Build
+echo "Building apteryx-netconf"
 export CFLAGS="-g -Wall -Werror -I$BUILD/usr/include"
 export LDFLAGS=-L$BUILD/usr/lib
-export PKG_CONFIG_PATH=$BUILD/usr/lib/pkgconfig
 if [ ! -f $BUILD/../Makefile ]; then
     cd $BUILD/../
     ./autogen.sh
-    ./configure APTERYX_XML_CFLAGS=-I$BUILD/usr/include APTERYX_XML_LIBS=-lapteryx-schema
+    ./configure \
+        APTERYX_CFLAGS=-I$BUILD/usr/include APTERYX_LIBS=-lapteryx \
+        APTERYX_XML_CFLAGS=-I$BUILD/usr/include APTERYX_XML_LIBS=-lapteryx-schema
     rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
     cd $BUILD
 fi
