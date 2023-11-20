d5b40f2df64a6b70873c3793063052007cdb866e
Tony van der Peet <tony.vanderpeet@alliedtelesis.co.nz>
2023-11-20 17:10:07 +1300

    Update run.sh

--- run.sh
+++ run.sh
@@ -132,7 +132,7 @@ rc=$?; if [[ $rc != 0 ]]; then quit $rc; fi
 # Start sshd
 sudo useradd -M -p $(perl -e 'print crypt($ARGV[0], "password")' 'friend') manager
 grep manager /etc/passwd
-socat -h
+socat -V
 ls -l $BUILD
 echo $(perl -e 'print crypt($ARGV[0], "password")' 'friend')
 netstat -l --tcp
